986916251f707423546d9a62ecb6df7b2daf0c2e
Maksym Chechkov <makmakom@gmail.com>
2021-06-06 13:05:06 +0300

    008. Student email, birthday added

--- students/models.py
+++ students/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 
@@ -8,3 +10,11 @@ class Student(models.Model):
     first_name = models.CharField(max_length=50, null=False)
     last_name = models.CharField(max_length=80, null=False)
     age = models.IntegerField(default=42)
+    email = models.EmailField(max_length=120, null=True)
+    birthday = models.DateField(default=datetime.date.today, null=True)
+
+    def __str__(self):
+        return f'{self.full_name()}, {self.birthday}'
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
