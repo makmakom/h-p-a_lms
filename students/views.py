45af66b6cc6bb4eb46ffbffa76dfef94c2c810c7
Maksym Chechkov <makmakom@gmail.com>
2021-06-10 21:28:22 +0300

    009. lesson. Forms. Linter

--- students/views.py
+++ students/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+
+from students.forms import StudentCreateForm
 from students.models import Student
 from django.shortcuts import render
+
 from webargs.djangoparser import use_args
 from webargs import fields
 
@@ -15,11 +19,9 @@ def hello(request):
 @use_args({
     "first_name": fields.Str(
         required=False,
-        missing='',
     ),
     "last_name": fields.Str(
         required=False,
-        missing='',
     ),
     "birthday": fields.Date(
         required=False,
@@ -30,10 +32,48 @@ def hello(request):
 def get_students(request, args):
     students = Student.objects.all()
     for param_name, param_value in args.items():
-        students = students.filter(**{param_name: param_value})
+        if param_value:
+            students = students.filter(**{param_name: param_value})
 
     records = format_records(students)
-    return HttpResponse(records)
+
+    html_form = """
+    <form action="" method="get">
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="first_name" ><br><br>
+        
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="last_name" ><br><br>
+        
+        <label>Age:</label>
+        <input type="number" name="age"><br><br>
+        
+        <input type="submit" value="Submit">
+    </form>
+    """
+
+    return HttpResponse(html_form + records)
+
+
+@csrf_exempt
+def create_student(request):
+    if request.method == 'POST':
+        form = StudentCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect('/students/')
+
+    form = StudentCreateForm()
+
+    html_form = f"""
+    <form method="post">
+      {form.as_p()}
+      <input type="submit" value="Submit">
+    </form>
+    """
+
+    return HttpResponse(html_form)
 
 
 def format_records(lst):
