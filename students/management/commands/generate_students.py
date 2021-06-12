c5770f2f4bac58336f4ab1bc1c53cd162c607873
Maksym Chechkov <makmakom@gmail.com>
2021-06-12 21:20:52 +0300

    008. task 3. Default count added

--- students/management/commands/generate_students.py
+++ students/management/commands/generate_students.py
@@ -7,12 +7,12 @@ class Command(BaseCommand):
     help = "Generate random students."
 
     def add_arguments(self, parser):
-        parser.add_argument('count', nargs='+', type=int)
+        parser.add_argument('count', nargs='?', default=10, type=int)
 
     def handle(self, *args, **options):
-        for count in options['count']:
-            try:
-                Student.generate_students(count)
-            except Exception:
-                raise CommandError('Cannot create students')
-            self.stdout.write(self.style.SUCCESS(f'{count} students were successfully created'))
+        count = options['count']
+        try:
+            Student.generate_students(count)
+        except Exception:
+            raise CommandError('Cannot create students')
+        self.stdout.write(self.style.SUCCESS(f'{count} students were successfully created'))
