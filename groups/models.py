df3b420f5662eea153793ad89b481010b45ff1fc
Maksym Chechkov <makmakom@gmail.com>
2021-06-14 22:18:35 +0300

    009. task 2. groups form added

--- groups/models.py
+++ groups/models.py
@@ -6,3 +6,6 @@ class Group(models.Model):
     start = models.DateField()
     lessons_count = models.IntegerField(default=12, null=False)
     lessons_passed = models.IntegerField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}, {self.start}, {self.lessons_count}, {self.lessons_passed}'
