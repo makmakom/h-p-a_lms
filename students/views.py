dbf7d5c04b1f8fc5f2ecbe3ba21bc3f2231115d4
Maksym Chechkov <makmakom@gmail.com>
2021-06-17 20:19:05 +0300

    0011. lesson. templates added

--- students/views.py
+++ students/views.py
@@ -35,52 +35,44 @@ def get_students(request, args):
         if param_value:
             students = students.filter(**{param_name: param_value})
 
-    records = format_records(students)
-
-    html_form = """
-    <form action="" method="get">
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="first_name" ><br><br>
-        
-        <label for="lname">Last name:</label>
-        <input type="text" id="lname" name="last_name" ><br><br>
-        
-        <label>Age:</label>
-        <input type="number" name="age"><br><br>
-        
-        <input type="submit" value="Search">
-    </form>
-    """
-
-    return HttpResponse(html_form + records)
+    return render(
+        request=request,
+        template_name='students/list.html',
+        context={
+            'students': students
+        }
+    )
 
 
 @csrf_exempt
 def create_student(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        form = StudentCreateForm()
+
+    elif request.method == 'POST':
         form = StudentCreateForm(request.POST)
         if form.is_valid():
             form.save()
 
             return HttpResponseRedirect('/students/')
 
-    form = StudentCreateForm()
-
-    html_form = f"""
-    <form method="post">
-      {form.as_p()}
-      <input type="submit" value="Create">
-    </form>
-    """
-
-    return HttpResponse(html_form)
+    return render(
+        request=request,
+        template_name='students/create.html',
+        context={
+            'form': form
+        }
+    )
 
 
 @csrf_exempt
 def update_student(request, id):
     student = Student.objects.get(id=id)
 
-    if request.method == 'POST':
+    if request.method == 'GET':
+        form = StudentUpdateForm(instance=student)
+
+    elif request.method == 'POST':
         form = StudentUpdateForm(
             data=request.POST,
             instance=student
@@ -90,23 +82,10 @@ def update_student(request, id):
 
             return HttpResponseRedirect('/students/')
 
-    form = StudentUpdateForm(instance=student)
-
-    html_form = f"""
-    <form method="post">
-      {form.as_p()}
-      <input type="submit" value="Save">
-    </form>
-    """
-
-    return HttpResponse(html_form)
-
-
-def format_records(lst):
-    if len(lst) == 0:
-        return '(Emtpy recordset)'
-    result = []
-    for elem in lst:
-        formatted = f'<a href="/students/update/{elem.id}">{elem.id}</a> {elem}'
-        result.append(formatted)
-    return '<br>'.join(result)
+    return render(
+        request=request,
+        template_name='students/update.html',
+        context={
+            'form': form
+        }
+    )
