eb19c9d99652c26eb4f8f98484f399b0e54d2967
Maksym Chechkov <makmakom@gmail.com>
2021-06-27 16:05:50 +0300

    generate commands added. groups, teachers

---
+++ teachers/management/commands/generate_teachers.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from teachers.models import Teacher
+
+
+class Command(BaseCommand):
+    help = "Generate random teachers."
+
+    def add_arguments(self, parser):
+        parser.add_argument('count', nargs='?', default=10, type=int)
+
+    def handle(self, *args, **options):
+        count = options['count']
+        try:
+            Teacher.generate_teachers(count)
+        except Exception:
+            raise CommandError('Cannot create teachers')
+        self.stdout.write(self.style.SUCCESS(f'{count} teachers were successfully created'))
