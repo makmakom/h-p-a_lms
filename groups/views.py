12e0cdf44f6ee9a0fce931826812b72d269d75fe
Maksym Chechkov <makmakom@gmail.com>
2021-07-05 23:02:39 +0300

    014. code cleaning

--- groups/views.py
+++ groups/views.py
@@ -5,29 +5,9 @@ from django.urls import reverse
 from groups.forms import GroupCreateForm, GroupUpdateForm, GroupsFilter
 from groups.models import Group
 
-from webargs import fields
-from webargs.djangoparser import use_args
 
-
-@use_args({
-    "name": fields.Str(
-        required=False,
-    ),
-    "lessons_count": fields.Int(
-        required=False,
-    ),
-    "lessons_passed": fields.Int(
-        required=False,
-    )
-},
-    location="query"
-)
-def get_groups(request, args):
+def get_groups(request):
     groups = Group.objects.all()
-    for param_name, param_value in args.items():
-        if param_value:
-            groups = groups.filter(**{param_name: param_value})
-
     obj_filter = GroupsFilter(data=request.GET, queryset=groups)
 
     return render(
