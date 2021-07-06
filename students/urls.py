954316dc081c055ea5a506019b4c0684a3fe83c7
Maksym Chechkov <mchechkov@gerc.ua>
2021-07-06 17:52:50 +0300

    015. ls. CBV added

--- students/urls.py
+++ students/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from students.views import create_student, delete_student, get_students, update_student
+from students.views import StudentUpdateView, create_student, delete_student, get_students
 
 app_name = 'students'
 
 urlpatterns = [
     path('', get_students, name='list'),
     path('create/', create_student, name='create'),
-    path('update/<int:pk>/', update_student, name='update'),
+    path('update/<int:pk>/', StudentUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', delete_student, name='delete'),
 ]
