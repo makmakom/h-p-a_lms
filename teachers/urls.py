fac68c93fbd0a42ca7f7ae86de0995f8403260b3
Maksym Chechkov <makmakom@gmail.com>
2021-07-06 18:26:20 +0300

    015. hw. teacher List -> CBV try

--- teachers/urls.py
+++ teachers/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from teachers.views import delete_teacher, get_teachers, TeacherUpdateView, TeacherCreateView
+from teachers.views import delete_teacher, TeacherUpdateView, TeacherCreateView, TeacherListView
 
 app_name = 'teachers'
 
 urlpatterns = [
-    path('', get_teachers, name='list'),
+    path('', TeacherListView.as_view(), name='list'),
     path('create/', TeacherCreateView.as_view(), name='create'),
     path('update/<int:pk>/', TeacherUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', delete_teacher, name='delete'),
