24b1aa7e445868967815d5d0e03025257c095d2d
Maksym Chechkov <makmakom@gmail.com>
2021-07-11 15:10:19 +0300

    017. lesson. adm

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
 
-# Register your models here.
+from .models import Group
+from students.models import Student
+
+
+class StudentsInlineTable(admin.TabularInline):
+    model = Student
+    fields = [
+        'last_name',
+        'first_name',
+        'email',
+        'age',
+    ]
+
+    # readonly_fields = fields
+    # show_change_link = True             # edit in Student model
+
+    extra = 0               # default = 3
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        # 'start_date',
+        # 'end_date',
+        'headman',
+        # 'days_of_week',
+    ]
+
+    fields = [
+        'name',
+        # ('start_date', 'end_date', 'days_of_week'),
+        'headman',
+        'teachers',
+    ]
+
+    inlines = [StudentsInlineTable]
+
+
+admin.site.register(Group, GroupAdmin)
