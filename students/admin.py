dd19c72a764f1d1da26eebc60d77f804389d9fc2
Maksym Chechkov <makmakom@gmail.com>
2021-07-11 14:50:03 +0300

    017. lesson. adm, messages

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,27 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
 
-# Register your models here.
+from students.models import Student
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = [
+        'first_name',
+        'last_name',
+        'birthdate',
+        'email',
+    ]
+    # list_display_links = list_display
+    list_per_page = 10
+    fields = [
+        ('first_name', 'last_name',),
+        ('birthdate', 'age',),
+        ('email',),
+        ('enroll_date', 'graduate_date',),
+        ('group',),
+    ]
+    readonly_fields = ['age']
+    search_fields = ['first_name']
+    list_filter = ['age']
+
+
+admin.site.register(Student, StudentAdmin)
