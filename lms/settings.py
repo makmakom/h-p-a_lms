8f519230546b8f972b53ad688d366f6a36a5f3e3
Maksym Chechkov <makmakom@gmail.com>
2021-07-11 15:10:19 +0300

    017. lesson. adm

--- lms/settings.py
+++ lms/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'core.middlewares.SimpleMiddleware',
 ]
 
 ROOT_URLCONF = 'lms.urls'
