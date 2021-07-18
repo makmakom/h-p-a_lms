dce06466241f986cfc9704528ad56e0129d5ba28
Maksym Chechkov <makmakom@gmail.com>
2021-07-18 11:10:18 +0300

    016. hw. students to CBV complete

--- students/views.py
+++ students/views.py
@@ -1,10 +1,18 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse, reverse_lazy
-from django.views.generic import UpdateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, ListView, CreateView, DeleteView
 
 from students.forms import StudentCreateForm, StudentUpdateForm, StudentsFilter
 from students.models import Student
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentCreateForm
+    success_url = reverse_lazy('students:list')
+    template_name = 'students/create.html'
+    extra_context = {
+        'title': 'Create student',
+    }
 
 
 class StudentListView(ListView):
@@ -21,43 +29,6 @@ class StudentListView(ListView):
         )
 
 
-def create_student(request):
-    if request.method == 'GET':
-        form = StudentCreateForm()
-    elif request.method == 'POST':
-        form = StudentCreateForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-            return HttpResponseRedirect(reverse('students:list'))
-
-    return render(
-        request=request,
-        template_name='students/create.html',
-        context={
-            'form': form,
-            'title': 'Create student',
-        }
-    )
-
-
-def delete_student(request, pk):
-    student = get_object_or_404(Student, id=pk)
-
-    if request.method == 'POST':
-        student.delete()
-        return HttpResponseRedirect(reverse('students:list'))
-
-    return render(
-        request=request,
-        template_name='students/delete.html',
-        context={
-            'student': student,
-            'title': 'Delete student',
-        }
-    )
-
-
 class StudentUpdateView(UpdateView):
     model = Student
     form_class = StudentUpdateForm
@@ -65,4 +36,13 @@ class StudentUpdateView(UpdateView):
     template_name = 'students/update.html'
     extra_context = {
         'title': 'Update student'
+    }
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('students:list')
+    template_name = 'students/delete.html'
+    extra_context = {
+        'title': 'Delete student',
     }
