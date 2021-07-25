6c5b20c8d20c426be3e8fa1ed81bc1ca08710cc4
Maksym Chechkov <makmakom@gmail.com>
2021-07-25 13:02:21 +0300

    019. ls. Profile media urs & path added

--- lms/urls.py
+++ lms/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from core.views import index
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import include, path
-
-from lms import settings
 
 urlpatterns = [
     path('', index, name='index'),
@@ -37,3 +37,5 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
